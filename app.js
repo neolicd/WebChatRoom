d3f2d8a7ad5c3fb2e73ef6b682cab73b08c1030f
neolicd <lichangdazju@yeah.net>
2016-07-30 14:32:09 +0800

    added socket.io module

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var express = require("express");
-var app = express();
+var app = require('express')();
+
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+
 
 app.get('/', function (req, res){
 	res.sendFile(__dirname+"/app.html");
 });
 
-app.listen(3000, function() {
+
+
+io.on('connection', function(socket){
+	socket.on('message', function(msg){
+		io.emit('message', msg);
+	});
+});
+
+
+http.listen(3000, function() {
 	console.log("The application is running on port:3000");
 });
+
