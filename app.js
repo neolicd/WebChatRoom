89515770ee19c7c5aed915ae61aa9f8f21892cde
neolicd <lichangdazju@yeah.net>
2016-07-31 14:47:50 +0800

    changed the code sytle to OOP

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var Chat = function() {
+	this.app = require('express')();
+	this.http = require('http').Server(this.app);
+	this.io = require('socket.io')(this.http);
+}
 
-
-
-app.get('/', function (req, res){
-	res.sendFile(__dirname+"/app.html");
-});
-
-
-
-io.on('connection', function(socket){
-	socket.on('message', function(msg){
-		io.emit('message', msg);
+Chat.prototype.setHomePage = function() {
+	this.app.get('/', function (req, res){
+		res.sendFile(__dirname+"/app.html");
 	});
-});
+};
+
+Chat.prototype.setBroadCast = function(io) {
+	io = this.io;
+	io.on('connection', function(socket){
+		socket.on('message', function(msg){
+			io.emit('message', msg);
+		});
+	});
+};
+
+Chat.prototype.listen = function() {
+	this.http.listen(3000, function() {
+		console.log("The application is running on port:3000");
+	});
+};
+
+var chat = new Chat();
+chat.setHomePage();
+var io;
+chat.setBroadCast(io);
+chat.listen();
 
 
-http.listen(4000, function() {
-	console.log("The application is running on port:4000");
-});
+
+
+
 
