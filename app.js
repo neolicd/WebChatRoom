a31c665ecb28031610d8c0ed40ba734304684001
neolicd <lichangdazju@yeah.net>
2016-08-02 14:17:56 +0800

    modified indetation

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 var Record = function() {
-	this.messages = {items:[]}; 
+    this.messages = {items:[]}; 
 };
 
 Record.prototype.add = function(item) {
-	var len = this.messages.items.length;
-	this.messages.items[len] = item;
+    var len = this.messages.items.length;
+    this.messages.items[len] = item;
 };
 
 Record.prototype.getItems = function() {
-	return this.messages.items;
+    return this.messages.items;
 }
 
 Record.prototype.read = function() {
-	var fs = require('fs');
-	var content = fs.readFileSync('record.json');
-	this.messages = JSON.parse(content);
+    var fs = require('fs');
+    var content = fs.readFileSync('record.json');
+    this.messages = JSON.parse(content);
 }
 
 
 Record.prototype.save = function() {
-	var fs = require('fs');
-	fs.writeFileSync('record.json', JSON.stringify(this.messages, null, 2));
+    var fs = require('fs');
+    fs.writeFileSync('record.json', JSON.stringify(this.messages, null, 2));
 }
 
 var Chat = function() {
-	this.app = require('express')();
-	this.http = require('http').Server(this.app);
-	this.io = require('socket.io')(this.http);
-	this.record = new Record();
+    this.app = require('express')();
+    this.http = require('http').Server(this.app);
+    this.io = require('socket.io')(this.http);
+    this.record = new Record();
 }
 
 Chat.prototype.setHomePage = function(fileName) {
-	this.app.get('/', function (req, res){
-		res.sendFile(__dirname + "/" + fileName);
-	});
+    this.app.get('/', function (req, res){
+        res.sendFile(__dirname + "/" + fileName);
+    });
 };
 
 Chat.prototype.setBroadCast = function() {
-	var __this = this;
-	__this.io.on('connection', function(socket){
-		var items = __this.record.getItems();
-		var len = items.length;
-		for(var i = 0; i < len; i++) {
-			socket.emit('message', items[i]);
-		}
+    var __this = this;
+    __this.io.on('connection', function(socket){
+        var items = __this.record.getItems();
+        var len = items.length;
+        for(var i = 0; i < len; i++) {
+            socket.emit('message', items[i]);
+        }
 
-		socket.on('message', function(item){
-			__this.record.add(item);
-			__this.io.emit('message', item);
-		});
-	});
+        socket.on('message', function(item){
+            __this.record.add(item);
+            __this.io.emit('message', item);
+        });
+    });
 };
 
 Chat.prototype.listen = function(portNum) {
-	this.http.listen(portNum, function() {
-		console.log("The application is running on port:" + portNum);
-	});
+    this.http.listen(portNum, function() {
+        console.log("The application is running on port:" + portNum);
+    });
 };
 
 
