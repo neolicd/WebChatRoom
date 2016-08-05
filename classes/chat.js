a8ea4a52b0b75ebc379b5537882fe47296b53017
neolicd <lichangdazju@yeah.net>
2016-08-05 17:43:24 +0800

    changed login's form sumbit method to post

--- classes/chat.js
+++ classes/chat.js
@@ -1,50 +1,54 @@
 var Chat = function() {};
 
 Chat.prototype.init = function() {
-	this.express = require('express');
-	this.app = this.express();
-	this.http = require('http').Server(this.app);
-	this.io = require('socket.io')(this.http);
-	this.record = new (require('./record.js'))();
-	this.app.use(this.express.static('public'));
-	this.app.set('view engine', 'ejs');
+    this.express = require('express');
+    this.app = this.express();
+    this.http = require('http').Server(this.app);
+    this.io = require('socket.io')(this.http);
+    this.record = new (require('./record.js'))();
+    this.app.use(this.express.static('public'));
+    this.app.set('view engine', 'ejs');
+
+    var bodyParser = require('body-parser');
+    this.app.use(bodyParser.urlencoded({extended:false}));
+    this.app.use(bodyParser.json());
 };
 
 Chat.prototype.setLoginPage = function(fileName) {
-	this.app.get('/', function(req, res) {
-		res.render(fileName);
-	});
+    this.app.get('/', function(req, res) {
+        res.render(fileName);
+    });
 };
 
 Chat.prototype.setMainPage = function(fileName) {
-	this.app.get('/main', function(req, res) {
-		res.render(fileName, {
-			uID: req.query.uID
-		});
-	});
+    this.app.post('/main', function(req, res) {
+        res.render(fileName, {
+            uID: req.body.uID
+        });
+    });
 };
 
 Chat.prototype.setBroadCast = function() {
-	var __this = this;
-	__this.io.on('connection', function(socket){
-		var items = __this.record.getItems();
-		var len = items.length;
-		for(var i = 0; i < len; i++) {
-			socket.emit('chat message', items[i]);
-		}
+    var __this = this;
+    __this.io.on('connection', function(socket){
+        var items = __this.record.getItems();
+        var len = items.length;
+        for(var i = 0; i < len; i++) {
+            socket.emit('chat message', items[i]);
+        }
 
-		socket.on('chat message', function(item){
-			__this.record.add(item);
-			//__this.record.save();
-			__this.io.emit('chat message', item);
-		});
-	});
+        socket.on('chat message', function(item){
+            __this.record.add(item);
+            //__this.record.save();
+            __this.io.emit('chat message', item);
+        });
+    });
 };
 
 Chat.prototype.listen = function(portNum) {
-	this.http.listen(portNum, function() {
-		console.log("The application is running on port:" + portNum);
-	});
+    this.http.listen(portNum, function() {
+        console.log("The application is running on port:" + portNum);
+    });
 };
 
 module.exports = Chat;
